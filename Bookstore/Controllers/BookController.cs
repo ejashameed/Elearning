52de056b36e4879102a00343681288ebcf1c1ad1
ejashameed <ejashameed@gmail.com>
2021-01-05 22:17:46 +0400

    Add Book Controller and repository

---
+++ Bookstore/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public ViewResult GetAllBooks()
+        {
+            var books = _bookRepository.GetAllBooks();
+            return View();
+        }
+
+        public BookModel GetBook(int Id)
+        {
+            return _bookRepository.GetBook(Id);
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return _bookRepository.SearchBooks(bookName, authorName);
+            
+        }
+    }
+}
---
+++ Bookstore/Repository/BookRepository.cs
@@ -0,0 +1,40 @@
+﻿using Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBook(int Id)
+        {
+            return DataSource().Where(x => x.Id == Id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBooks(string title, string author)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+              {
+                new BookModel(){Id = 1, Title = "MVC Book", Author="Ejas Hameed"},
+                new BookModel(){Id = 2, Title = "REST API Book", Author="Bruce Willis"},
+                new BookModel(){Id = 3, Title = "Java", Author="Mark Antony"},
+                new BookModel(){Id = 4, Title = "C#", Author="Bill Gates"},
+                new BookModel(){Id = 5, Title = "Python", Author="Cobra"},
+                new BookModel(){Id = 6, Title = "Azure Cloud", Author="Thunder"},
+                new BookModel(){Id = 7, Title = "AWS", Author="Jeff Bezos"},
+              };
+        }
+
+    }
+}
